8e395e988c6d2508e2d44dcc38a9b497ae411609
Ben Sparrow <bensparrowdev@gmail.com>
2021-03-29 17:49:59 +0100

    end of day commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const  app = express();
 
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    console.log(req.query);
+    res.send({ message: "Hello mate", date: "29th March 2021"});
+});
+
+app.get("/profile", (req, res) => {
+    res.send({message: `hello  my name is ${req.query.name}`})
+    //localhost:5000/profile/?name=ben     this is the query for name
+});
+
+app.get("/person/:id", (req, res) => {
+    console.log(req.params.id);
+
+    res.send({ message: "hello" })
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+
+    res.send({ data: req.body })
+});
 
 app.listen(5000, () => {
     console.log("listening on port 5000");
